a9f1f7817f653e46470c5ebb49a6b3e00579cc92
Ravula Sai <96985470+ravula07@users.noreply.github.com>
2023-04-29 03:42:40 +0530

    Update demo_ui.py

--- demo_ui.py
+++ demo_ui.py
@@ -18,17 +18,7 @@ tab1, tab2 = st.tabs(["CHAT HERE 💬", " Uploaded Documents 📄"])
 with st.sidebar:
     st.title("Upload Your Birdie")
     uploaded_file = st.file_uploader("Choose a file", type=["png", "jpg"])
-    clear_button = st.button("Clear Conversation", key="clear")
-
-if uploaded_file is not None:
-  save_uploadedfile(uploaded_file)
-  PDF_loader("tempfolder/" + uploaded_file.name)
-  tab1.markdown(
-        "<h3 style='text-align: center;'>Now You Are Talking With "
-        + uploaded_file.name
-        + "</h3>",
-        unsafe_allow_html=True,
-    )   
+    clear_button = st.button("Clear Conversation", key="clear") 
     
 with tab1:
     user_input = st.text_area(label=":green[Welcome to CHIRP CHAT! Type in your bird-brained questions]")
