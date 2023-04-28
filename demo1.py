0c056649539a9be313fc0a8e699b1cc3a3830fda
Ravula Sai <96985470+Kakashi-07@users.noreply.github.com>
2023-04-28 10:01:48 +0530

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,7 +1,5 @@
 import streamlit as st
-from langchain.document_loaders.image import UnstructuredImageLoader
-from langchain.vectorstores import Chroma
-from langchain.prompts import PromptTemplate
+
 
 st.set_page_config(page_title="Endangered Bird Voice Identifier")
 
