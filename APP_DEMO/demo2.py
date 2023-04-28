536c237b33694aedf90c5ac7018734f72bde45df
Ravula Sai <96985470+ravula07@users.noreply.github.com>
2023-04-29 04:30:37 +0530

    Update demo2.py

--- APP_DEMO/demo2.py
+++ APP_DEMO/demo2.py
@@ -1,125 +1,107 @@
 import numpy as np
+from PIL import Image
 import streamlit as st
 import tensorflow as tf
-from skimage import transform
-from langchain import Cohere, ConversationChain, LLMChain, PromptTemplate
 from langchain.memory import ConversationBufferWindowMemory
-from keras.preprocessing import image
+from langchain import Cohere, ConversationChain, LLMChain, PromptTemplate
 from keras.applications.vgg16 import preprocess_input, decode_predictions, VGG16
 
-st.set_page_config(page_title="Endangered Bird Voice Identifier")
+st.set_page_config(page_title="CHIRP CHAT",
+                  page_icon="🐥",
+                  layout="wide",
+                  initial_sidebar_state="expanded")
 
-tab1= st.tabs(["📈 Talk Here"])
+#st.title(':orange[CHIRP CHAT]')
+#st.markdown("<h1 style='text-align: center;'>:orange[CHIRP CHAT]</h1>", unsafe_allow_html=True,)
+st.markdown("<h1 style='text-align: center; color: orange;'>CHIRP CHAT 🐥 </h1> ", unsafe_allow_html=True)
 
-st.sidebar.title("SeLeCt YoUr ChOiCe")
-options = [
-    "Image Recognition",
-]
+tab1, tab2 = st.tabs(["CHAT HERE 💬", " Uploaded Documents 📄"])
 
-choice = st.sidebar.radio("Select the method you want", options)
-
-if choice == "Image Recognition":
-    Uploaded_file = st.file_uploader(
-        "Upload the Image File", type=["jpg", "png"])
-    if Uploaded_file is None:
-        st.header(" File format not supported!! ")
-    st.image(Uploaded_file, caption='YOUR IMAGE')
+# Creating Sidebar for Utilites
+with st.sidebar:
+    st.title("Upload Your Birdie")
+    uploaded_file = st.file_uploader("Choose a file", type=["png", "jpg"])
+    clear_button = st.button("Clear Conversation", key="clear") 
     
-user_input = st.text_input("You:", key="input")
+#with tab1:
+    #user_input = st.text_area(label=":green[Welcome to CHIRP CHAT! Type in your bird-brained questions]")
+     
+with tab2:
+   if uploaded_file is not None:
+    uploaded_image = Image.open(uploaded_file)
+    x = preprocess_input(np.array(uploaded_image))
+    model = VGG16(weights='imagenet')
+    #img = uploaded_file
+    #x = np.array(img)
+    x = np.expand_dims(x, axis=0)
+    preds = model.predict(x)
     
-model = VGG16(weights='imagenet')
+    decoded_preds = decode_predictions(preds, top=1)[0]
+    for pred in decoded_preds:
+      result = pred[1]
+    st.image(uploaded_file, caption=result)
 
-img = Uploaded_file
-#img = cv2.resize(img,(224, 224))
-#img = transform.resize(img, (256, 256))
-x = np.array(img)
-x = np.expand_dims(x, axis=0)
-#x = preprocess_input(x)
+with tab1:
+    if uploaded_file is not None:
+        tab1.markdown("<h3 style='text-align: center;'>Now you're chirping about the bird "
+        + result
+        + "</h3>",
+        unsafe_allow_html=True,)
 
-# Make predictions
-preds = model.predict(x)
-
-# Decode the predictions
-decoded_preds = decode_predictions(preds, top=1)[0]
-print('Predictions:')
-for pred in decoded_preds:
-     result = pred[1]
-     st.subheader(result)
-    
 def Img_DataGen(Input):
-    template = """
-    You are an AI Chatbot developed solely to help users to talk about Birds and their information.
-    
-    {history}
-    Human: {human_input}
-    Assistant:"""
+  cohere_api_key= st.secrets["cohere_apikey"]
+  LLM = Cohere(cohere_api_key=cohere_api_key , model="command-xlarge-nightly")
+  
+  prompt1 = PromptTemplate(
+  input_variables=["product"],
+  template="Generate a paragraph about {product} as if an Ornithologist is saying it.")
+  chain = LLMChain(llm=LLM, prompt=prompt1)
+  var = chain.run(result)
+  
+  
+  template = """
+  You are an AI Chatbot who acts as a professional Ornithologist. You are designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions. As a language model, you must be able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+  Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+  Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+  your task is answer any question asked about {result}. some basic info on {result} is: {var}
+  """
+  
+  prompt = PromptTemplate(
+  input_variables=["result","var"], 
+  template=template)
+  var = prompt.format(result=result, var = var)
+  
+  var = var + """{history}
+  Human: {human_input}
+  Assistant:"""
+  
+  prompt = PromptTemplate(
+  input_variables=["history", "human_input"], 
+  template=var)
 
-    prompt1 = PromptTemplate(
-    input_variables=["product"],
-    template="Generate a paragraph about {product} as if an Ornithologist is saying it.",
-    )
-    chain = LLMChain(llm=LLM, prompt=prompt1)
-    var = chain.run(result)
-    
-    template = """
-        You are an AI Chatbot who acts as a professional Ornithologist. You are designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions. As a language model, you must be able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
-        Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
-        Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
-        your task is answer any question asked about {result}. some basic info on {result} is: {var}
-        """
+  global chat_chain
+  chat_chain = LLMChain(
+  llm = Cohere(cohere_api_key= COHERE_API_KEY, model="command-xlarge-nightly"), 
+  prompt=prompt, 
+  verbose=True, 
+  memory=ConversationBufferWindowMemory(k=8)
+  )
+  return "Ready"
 
-    prompt = PromptTemplate(
-    input_variables=["result","var"], 
-    template=template
-    )
-    
-    var = var + """{history}
-    Human: {human_input}
-    Assistant:"""
-
-    var = prompt.format(result=result, var = var)
-    
-    global chat
-    prompt = PromptTemplate(
-    input_variables=["history", "human_input"], 
-    template=var
-    )
-
-    chat_chain = LLMChain(
-    llm = Cohere(cohere_api_key= st.secrets["cohere_apikey"], model="command-xlarge-nightly"), 
-    prompt=prompt, 
-    verbose=True, 
-    memory=ConversationBufferWindowMemory(k=8),
-    )
-    
-    return "Ready"
-
-if Uploaded_file is not None:
-    save_uploadedfile(Uploaded_file)
-    Img_DataGen(result)
-    
-
- # Session State
+# Session State
 if "chat_history" not in st.session_state:
     st.session_state["chat_history"] = []
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
 if "past" not in st.session_state:
     st.session_state["past"] = []
-
-# Generating Response
+   
 def generate_response(query):
-    query = user_input
-    if user_input:
-        res = chat.predict(human_input = query)
-        res = chat({"query": query, "chat_history": st.session_state["chat_history"]})
-        res["res"] = res["res"]
-        return res["res"]
+    result = chat_chain({"query": query})
 
-
+# Creating Containers
 response_container = tab1.container()
 container = tab1.container()
-
 
 with container:
     with st.form(key="my_form", clear_on_submit=True):
@@ -136,8 +118,22 @@ with container:
         else:
             st.session_state["past"].append(user_input)
             st.session_state["generated"].append(
-                "Please upload the picture!"
+                "Hey, you there! Don't be a bird-brain and upload a picture of a birdie in the sidebar."
             )
+
+if st.session_state["generated"]:
+    with response_container:
+        for i in range(len(st.session_state["generated"])):
+            message(
+                st.session_state["past"][i],
+                is_user=True,
+                key=str(i) + "_user",
+                avatar_style="adventurer",
+                seed=123,
+            )
+            message(st.session_state["generated"][i], key=str(i))
+
+# Enabling Clear button
 
 if clear_button:
     st.session_state["generated"] = []
