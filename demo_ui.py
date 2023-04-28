a3fe185dc0af99d1668675babf846f5b307a8eaf
Ravula Sai <96985470+ravula07@users.noreply.github.com>
2023-04-29 01:45:30 +0530

    Update demo_ui.py

--- demo_ui.py
+++ demo_ui.py
@@ -5,13 +5,16 @@ st.set_page_config(page_title="CHIRP CHAT",
                   layout="wide",
                   initial_sidebar_state="expanded")
 
-st.title(':orange[CHIRP CHAT]')
-user_input = st.text_area(label=":green[Welcome to CHIRP CHAT! Type in your bird-brained questions]")
+#st.title(':orange[CHIRP CHAT]')
+st.markdown("<h1 style='text-align: center;'>:orange[CHIRP CHAT]</h1>", unsafe_allow_html=True,)
 
 # tabs
-tab1, tab2 = st.tabs(["📈 CHAT HERE ", "🗃 Uploaded Documents"])
+tab1, tab2 = st.tabs(["CHAT HERE 💬", " Uploaded Documents 📄"])
 
 tab1.markdown(
     "<h1 style='text-align: center;'>Talk With CHIRP CHAT</h1>",
     unsafe_allow_html=True,
 )
+
+user_input = st.text_area(label=":green[Welcome to CHIRP CHAT! Type in your bird-brained questions]")
+
