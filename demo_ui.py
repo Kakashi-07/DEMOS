55eb37bc60390acdfb120091606f80d4ef541ccd
Ravula Sai <96985470+ravula07@users.noreply.github.com>
2023-04-29 01:37:30 +0530

    Update demo_ui.py

--- demo_ui.py
+++ demo_ui.py
@@ -7,3 +7,15 @@ st.set_page_config(page_title="CHIRP CHAT",
 
 st.title(':orange[CHIRP CHAT]')
 user_input = st.text_area(label=":green[Welcome to CHIRP CHAT! Type in your bird-brained questions]")
+
+if not os.path.exists("./tempfolder"):
+    os.makedirs("./tempfolder")
+
+
+# tabs
+tab1, tab2 = st.tabs(["📈 CHAT HERE ", "🗃 Uploaded Documents"])
+
+tab1.markdown(
+    "<h1 style='text-align: center;'>Talk With CHIRP CHAT</h1>",
+    unsafe_allow_html=True,
+)
