a198b0d9141a53668fb40f205dc4430573178774
Ravula Sai <96985470+ravula07@users.noreply.github.com>
2023-04-29 01:28:45 +0530

    Update demo_ui.py

--- demo_ui.py
+++ demo_ui.py
@@ -5,5 +5,5 @@ st.set_page_config(page_title="CHIRP CHAT",
                   layout="wide",
                   initial_sidebar_state="expanded")
 
-st.title('orange[CHIRP CHAT]')
+st.title(':orange[CHIRP CHAT]', text_align="center")
 user_input = st.text_area(label=":green[Welcome to CHIRP CHAT! Type in your bird-brained questions]")
