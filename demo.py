a53c5f516824ee5827c2a0eb5ef76e0368d4c8c9
Ravula Sai <96985470+Kakashi-07@users.noreply.github.com>
2023-04-28 10:02:12 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,71 +1,49 @@
-# Import Required Libraries
-import os
 import streamlit as st
-from langchain.document_loaders import OnlinePDFLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.document_loaders.image import UnstructuredImageLoader
 from langchain.vectorstores import Chroma
-from langchain.chains import RetrievalQA
-from langchain.embeddings import CohereEmbeddings
 from langchain.prompts import PromptTemplate
-from langchain.llms import Cohere
 
-# Setting Up API Tokens
-# Create .streamlit Folder in Root Directory
-# Create a File secrets.toml
-# TOML format
-# cohere_apikey="Enter you Key"
+st.set_page_config(page_title="Endangered Bird Voice Identifier")
 
+st.sidebar.title("SeLeCt YoUr ChOiCe")
+options = [
+    "Image Recognition",
+    "Audio Recognition",
+]
 
-# Setting Up Streamlit Page
-st.set_page_config(page_title="Talk With PDF", page_icon=":smile:")
+choice = st.sidebar.radio("Select the method you want", options)
 
+if choice == "Image Recognition":
+    Uploaded_file = st.file_uploader(
+        "Upload the Image File", type=["jpg", "png"])
+    if Uploaded_file is None:
+        st.header(" File format not supported!! ")
 
-# Creating Temp Folder
-if not os.path.exists("./tempfolder"):
-    os.makedirs("./tempfolder")
+    st.image(Uploaded_file, caption='YOUR IMAGE')
 
+elif choice == "Audio Recognition":
+    Uploaded_file = st.file_uploader(
+        "Upload the Image File", type=["mp3", "wav"])
+    if Uploaded_file is None:
+        st.header(" File format not supported!! ")
 
-# tabs
-tab1, tab2 = st.tabs(["📈 Talk Here", "🗃 Relevant Documents"])
-
-tab1.markdown(
-    "<h1 style='text-align: center;'>Talk With PDF</h1>",
-    unsafe_allow_html=True,
-)
-
-
-# Saving Upload file to tempfolder
-def save_uploadedfile(uploadedfile):
-    with open(
-        os.path.join("tempfolder", uploadedfile.name),
-        "wb",
-    ) as f:
-        f.write(uploadedfile.getbuffer())
-    return st.sidebar.success("Saved File")
-
-
-# Creating Sidebar for Utilites
-with st.sidebar:
-    st.title("Upload PDF")
-    uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
-    temp_r = st.slider("Temperature", 0.1, 0.9, 0.3, 0.1)
-    chunksize = st.slider("Chunk Size for Splitting Document ", 256, 1024, 300, 10)
-    clear_button = st.button("Clear Conversation", key="clear")
-
-# Initialzing Text Splitter
-text_splitter = CharacterTextSplitter(chunk_size=chunksize, chunk_overlap=10)
-
-# Intializing Cohere Embdedding
-embeddings = CohereEmbeddings(model="large", cohere_api_key=st.secrets["cohere_apikey"])
-
-
+    audio_bytes = Uploaded_file.read()
+    st.audio(Uploaded_file)
+    sample_rate = 44100  # 44100 samples per second
+    seconds = 2  # Note duration of 2 seconds
+    frequency_la = 440  # Our played note will be 440 Hz
+    # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+    t = np.linspace(0, seconds, seconds * sample_rate, False)
+    # Generate a 440 Hz sine wave
+    note_la = np.sin(frequency_la * t * 2 * np.pi)
+    st.audio(note_la, sample_rate=sample_rate)
+    
 def PDF_loader(document):
     loader = OnlinePDFLoader(document)
     documents = loader.load()
     prompt_template = """ 
     Your are an AI Chatbot devolped to help users to talk to a PDF document.Use the following pieces of context to answer the question at the end.Greet Users!!
     {context}
-
     {question}
     """
     PROMPT = PromptTemplate(
@@ -89,80 +67,3 @@ def PDF_loader(document):
         chain_type_kwargs=chain_type_kwargs,
     )
     return "Ready"
-
-
-if uploaded_file is not None:
-    save_uploadedfile(uploaded_file)
-    PDF_loader("tempfolder/" + uploaded_file.name)
-    tab1.markdown(
-        "<h3 style='text-align: center;'>Now You Are Talking With "
-        + uploaded_file.name
-        + "</h3>",
-        unsafe_allow_html=True,
-    )
-
-# Session State
-if "chat_history" not in st.session_state:
-    st.session_state["chat_history"] = []
-if "generated" not in st.session_state:
-    st.session_state["generated"] = []
-if "past" not in st.session_state:
-    st.session_state["past"] = []
-
-
-# Generating Response
-def generate_response(query):
-    result = qa({"query": query, "chat_history": st.session_state["chat_history"]})
-
-    tab2.markdown(
-        "<h3 style='text-align: center;'>Relevant Documents Metadata</h3>",
-        unsafe_allow_html=True,
-    )
-
-    tab2.write(result["source_documents"])
-    result["result"] = result["result"]
-    return result["result"]
-
-
-# Creating Containers
-
-response_container = tab1.container()
-container = tab1.container()
-
-
-with container:
-    with st.form(key="my_form", clear_on_submit=True):
-        user_input = st.text_input("You:", key="input")
-        submit_button = st.form_submit_button(label="Send")
-
-    if user_input and submit_button:
-        if uploaded_file is not None:
-            output = generate_response(user_input)
-            print(output)
-            st.session_state["past"].append(user_input)
-            st.session_state["generated"].append(output)
-            st.session_state["chat_history"] = [(user_input, output)]
-        else:
-            st.session_state["past"].append(user_input)
-            st.session_state["generated"].append(
-                "Please go ahead and upload the PDF in the sidebar, it would be great to have it there."
-            )
-
-if st.session_state["generated"]:
-    with response_container:
-        for i in range(len(st.session_state["generated"])):
-            message(
-                st.session_state["past"][i],
-                is_user=True,
-                key=str(i) + "_user",
-                avatar_style="adventurer",
-                seed=123,
-            )
-            message(st.session_state["generated"][i], key=str(i))
-
-# Enabling Clear button
-
-if clear_button:
-    st.session_state["generated"] = []
-    st.session_state["past"] = []
-    st.session_state["chat_history"] = []
