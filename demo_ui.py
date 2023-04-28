76b3f1f5571708d8202ca35d8514878897edde7d
Ravula Sai <96985470+ravula07@users.noreply.github.com>
2023-04-29 01:12:22 +0530

    Update demo_ui.py

--- demo_ui.py
+++ demo_ui.py
@@ -4,3 +4,5 @@ st.set_page_config(page_title="CHIRP CHAT",
                   page_icon="🐥",
                   layout="wide",
                   initial_sidebar_state="expanded")
+
+st.text(:green[Howdy! Type in your bird-brained questions here!])
