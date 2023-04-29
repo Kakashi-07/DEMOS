7e5b40044ecb912573175e0dff64869defb9c1b5
Ravula Sai <96985470+ravula07@users.noreply.github.com>
2023-04-29 10:32:35 +0530

    Update demo2.py

--- APP_DEMO/demo2.py
+++ APP_DEMO/demo2.py
@@ -48,44 +48,42 @@ with tab1:
         + "</h3>",
         unsafe_allow_html=True,)
 
-  cohere_api_key= "kDWTfFn3bcbnIcfbW4X6u7WyCxr3yHjAmFeRnUcT"
-  LLM = Cohere(cohere_api_key=cohere_api_key , model="command-xlarge-nightly")
+cohere_api_key= "kDWTfFn3bcbnIcfbW4X6u7WyCxr3yHjAmFeRnUcT"
+LLM = Cohere(cohere_api_key=cohere_api_key , model="command-xlarge-nightly")
+prompt1 = PromptTemplate(
+input_variables=["product"],
+template="Generate a paragraph about {product} as if an Ornithologist is saying it.")
+chain = LLMChain(llm=LLM, prompt=prompt1)
+var = chain.run(result)
   
-  prompt1 = PromptTemplate(
-  input_variables=["product"],
-  template="Generate a paragraph about {product} as if an Ornithologist is saying it.")
-  chain = LLMChain(llm=LLM, prompt=prompt1)
-  var = chain.run(result)
-  
-  
-  template = """
+template = """
   You are an AI Chatbot who acts as a professional Ornithologist. You are designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions. As a language model, you must be able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
   Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
   Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
   your task is answer any question asked about {result}. some basic info on {result} is: {var}
   """
   
-  prompt = PromptTemplate(
-  input_variables=["result","var"], 
-  template=template)
-  var = prompt.format(result=result, var = var)
+prompt = PromptTemplate(
+input_variables=["result","var"], 
+template=template)
+var = prompt.format(result=result, var = var)
   
-  var = var + """{history}
-  Human: {human_input}
-  Assistant:"""
+var = var + """{history}
+Human: {human_input}
+Assistant:"""
   
-  prompt = PromptTemplate(
-  input_variables=["history", "human_input"], 
-  template=var)
+prompt = PromptTemplate(
+input_variables=["history", "human_input"], 
+template=var)
 
-  global chat_chain
-  chat_chain = LLMChain(
-  llm = Cohere(cohere_api_key= COHERE_API_KEY, model="command-xlarge-nightly"), 
-  prompt=prompt, 
-  verbose=True, 
-  memory=ConversationBufferWindowMemory(k=8)
-  )
-  return "Ready"
+global chat_chain
+chat_chain = LLMChain(
+llm = Cohere(cohere_api_key= COHERE_API_KEY, model="command-xlarge-nightly"), 
+prompt=prompt, 
+verbose=True, 
+memory=ConversationBufferWindowMemory(k=8)
+)
+return "Ready"
 
 # Session State
 if "chat_history" not in st.session_state:
@@ -95,8 +93,8 @@ if "generated" not in st.session_state:
 if "past" not in st.session_state:
     st.session_state["past"] = []
    
-#def generate_response(query):
-    #resu = chat_chain({"query": query})
+def generate_response(query):
+    resu = chat_chain({"query": query})
 
 # Creating Containers
 response_container = tab1.container()
@@ -109,7 +107,7 @@ with container:
 
     if user_input and submit_button:
         if uploaded_file is not None:
-            output = chat_chain(user_input)
+            output = generate_response(user_input)
             print(output)
             st.session_state["past"].append(user_input)
             st.session_state["generated"].append(output)
